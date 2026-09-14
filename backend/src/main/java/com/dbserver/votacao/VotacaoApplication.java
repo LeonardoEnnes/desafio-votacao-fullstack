e2e13aa459fd6128c647d2c6c86e6e6b79d524ee
Leonardo Ennes <leonardoennes7@gmail.com>
2026-09-14 16:54:20 -0300

    chore: add Dockerfile, docker-compose and dockerignore

--- backend/src/main/java/com/dbserver/votacao/VotacaoApplication.java
+++ backend/src/main/java/com/dbserver/votacao/VotacaoApplication.java
@@ -1,4 +1,4 @@
-package com.db.votacao;
+package com.dbserver.votacao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- backend/src/test/java/com/dbserver/votacao/VotacaoApplicationTests.java
+++ backend/src/test/java/com/dbserver/votacao/VotacaoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.db.votacao;
+package com.dbserver.votacao;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
